223183656b1ba6e3f628d595875c2f65c0e2168a
Rafal <rafal@szyszka.it>
2017-07-04 12:08:39 +0200

    First layouts

---
+++ app/src/main/java/it/szyszka/skillmarket/SignInActivity.java
@@ -0,0 +1,13 @@
+package it.szyszka.skillmarket;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.module_a_sign_in);
+    }
+}
