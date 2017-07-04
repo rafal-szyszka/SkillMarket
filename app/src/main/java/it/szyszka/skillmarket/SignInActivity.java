73ad369760dff8730a68e863620549719609da41
Rafal <rafal@szyszka.it>
2017-07-04 16:36:23 +0200

    some more layouts

--- app/src/main/java/it/szyszka/skillmarket/SignInActivity.java
+++ app/src/main/java/it/szyszka/skillmarket/SignInActivity.java
@@ -2,12 +2,29 @@ package it.szyszka.skillmarket;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SignInActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.module_a_sign_in);
+        setContentView(R.layout.module_b_user_profile);
+
+        View view = findViewById(R.id.user_profile_email);
+        ImageView img = view.findViewById(R.id.user_info_img);
+        TextView txt = view.findViewById(R.id.user_info_text);
+
+        txt.setText("rafal@szyszka.it");
+
+        view = findViewById(R.id.user_profile_location);
+        img = view.findViewById(R.id.user_info_img);
+        img.setImageResource(R.drawable.ic_location);
+        txt = view.findViewById(R.id.user_info_text);
+        txt.setText("Wrocław");
+
     }
 }
